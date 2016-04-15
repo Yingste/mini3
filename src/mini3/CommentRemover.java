a0299b31438d97f7460c801e7bc431588f4e6914
Eric <yingsteric@gmail.com>
2016-04-14 23:12:14 -0500

    init upload

---
+++ src/mini3/CommentRemover.java
@@ -0,0 +1,47 @@
+package mini3;
+
+public class CommentRemover {
+
+	
+	
+	public CommentRemover()
+	{
+		
+	}
+	
+	
+	
+	public java.lang.String apply(java.lang.String s)
+	{
+		return s;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
---
+++ src/mini3/NonCommentLineSelector.java
@@ -0,0 +1,51 @@
+package mini3;
+
+public class NonCommentLineSelector {
+
+	
+	
+	public NonCommentLineSelector()
+	{
+		
+	}
+	
+	
+	public boolean select(java.lang.String s)
+	{
+		return false;
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
